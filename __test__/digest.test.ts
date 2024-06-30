00c49366f078d7bf311a2b79a79ab734d42bbde8
ICHINOSE Shogo <shogo82148@gmail.com>
2024-06-30 16:26:43 +0900

    add test case for sha512 digest

--- __test__/digest.test.ts
+++ __test__/digest.test.ts
@@ -11,4 +11,14 @@ describe("calculateDigest", () => {
       "sha256:f0c641fce0310cb43208f4272dc1423153630e44ba8cdc0b5ce8466f724e0eed"
     );
   });
+
+  it("should calculate sha512 digest", async () => {
+    const digest = await calculateDigest(
+      path.join(__dirname, "sigstore-js-2.1.0.tgz"),
+      "sha512"
+    );
+    expect(digest).toBe(
+      "sha512:90f223f992e4c88dd068cd2a5fc57f9d2b30798343dd6e38f29c240e04ba090ef831f84490847c4e82b9232c78e8a258463b1e55c0f7469f730265008fa6633f"
+    );
+  });
 });
