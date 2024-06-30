689e96c62ac9e0ec6ce4aeb66ed7f287bb745786
ICHINOSE Shogo <shogo82148@gmail.com>
2024-06-30 19:59:31 +0900

    implement verification

---
+++ src/verification.ts
@@ -0,0 +1,18 @@
+import { toSignedEntity, toTrustMaterial, Verifier } from "@sigstore/verify";
+import { bundleFromJSON } from "@sigstore/bundle";
+import * as tuf from "@sigstore/tuf";
+
+const GitHubOidcIssuer = "https://token.actions.githubusercontent.com";
+
+export async function verifyBundle(obj: unknown) {
+  const bundle = bundleFromJSON(obj);
+  const signedEntity = toSignedEntity(bundle);
+
+  const trustedRoot = await tuf.getTrustedRoot();
+  const trustMaterial = toTrustMaterial(trustedRoot);
+  const verifier = new Verifier(trustMaterial);
+
+  verifier.verify(signedEntity, {
+    extensions: { issuer: GitHubOidcIssuer },
+  });
+}
