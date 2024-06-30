0ba17f8e35c184eb7bd83af698b83fa07a02c7c3
ICHINOSE Shogo <shogo82148@gmail.com>
2024-06-30 16:14:57 +0900

    use import instead of require

--- __test__/digest.test.ts
+++ __test__/digest.test.ts
@@ -1,12 +1,12 @@
-const digest = require("../src/digest");
+import { calculateDigest } from "../src/digest";
 
 describe("calculateDigest", () => {
   it("should calculate sha256 digest", async () => {
-    const _digest = await digest.calculateDigest(
+    const digest = await calculateDigest(
       "__test__/sigstore-js-2.1.0.tgz",
       "sha256"
     );
-    expect(_digest).toBe(
+    expect(digest).toBe(
       "sha256:f0c641fce0310cb43208f4272dc1423153630e44ba8cdc0b5ce8466f724e0eed"
     );
   });
